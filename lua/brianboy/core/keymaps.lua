7cdd33fc55ee5b03a62920ab777c64a1de952c0f
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-06-05 06:53:48 +0200

    wip: Add keymaps for easier write and quit as well as copy in visual mode

--- lua/brianboy/core/keymaps.lua
+++ lua/brianboy/core/keymaps.lua
@@ -10,6 +10,13 @@ keymap.set({ 'i', 'v', 'c' }, 'kj', '<ESC>', { desc = 'Exit insert mode with jk'
 keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
 keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
+-- greatest remap ever
+keymap.set('x', '<leader>p', [["_dP]])
+
+-- File & window management
+keymap.set('n', '<leader>w', ':wa<enter>', { desc = 'Write all buffers', noremap = false })
+keymap.set('n', '<leader>q', ':wqa<enter>', { desc = 'Write and quit all buffers', noremap = false })
+
 -- Center cursor on linewrap, halfpage jump and search next/prev
 keymap.set('n', 'J', 'mzJ`z')
 keymap.set('n', '<C-d>', '<C-d>zz')
@@ -17,25 +24,19 @@ keymap.set('n', '<C-u>', '<C-u>zz')
 keymap.set('n', 'n', 'nzzzv')
 keymap.set('n', 'N', 'Nzzzv')
 
-keymap.set('n', '<Esc', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
+keymap.set('n', '<leader><leader>', '<cmd>set nohlsearch<CR>', { desc = 'Clear search highlights' })
 
 -- increment/decrement numbers
-keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number' }) -- increment
-keymap.set('n', '<leader>-', '<C-x>', { desc = 'Decrement number' }) -- decrement
+keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number' })
+keymap.set('n', '<leader>-', '<C-x>', { desc = 'Decrement number' })
 
 keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- window management
-keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' }) -- split window vertically
-keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' }) -- split window horizontally
-keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' }) -- make split windows equal width & height
-keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' }) -- close current split window
-
---[[ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab ]]
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
 
 keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
